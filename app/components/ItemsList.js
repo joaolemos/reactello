dd0b6bd21849c3b80c810415ca137d6fa451fdd2
João Lemos <joao.n.lemos@gmail.com>
2016-06-25 16:47:17 +0100

    New button for adding items...

--- app/components/ItemsList.js
+++ app/components/ItemsList.js
@@ -18,6 +18,16 @@ function ItemsList(props) {
           {items}
         </ul>
       </div>
+      <div className="panel-footer">
+        <div className="input-group">
+          <input type="text" className="form-control" placeholder="New item..." />
+          <span className="input-group-btn">
+            <button className="btn btn-default" type="button">
+                <span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
+            </button>
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
